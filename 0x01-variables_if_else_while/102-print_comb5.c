0a91cbb6bc5995a47e26bb7487c24e226123115c
LoraVan2 <vanderpuyeloretta22@gmail.com>
2023-03-18 15:53:27 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -16,14 +16,14 @@ int main(void)
 	for (p = 0; p <= 98; p++)
 	{
 
-	for (q = 0; q <= 98; q++)
+	for (q = p + 1; q <= 99; q++)
 	{
 	putchar((p / 10) + '0');
 	putchar((p % 10) + '0');
 	putchar(' ');
 	putchar((q / 10) + '0');
 	putchar((q % 10) + '0');
-	if (p == 98 && q == 98)
+	if (p == 98 && q == 99)
 	continue;
 	putchar(',');
 	putchar(' ');
