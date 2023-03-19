c83efe41dbe8c50c4a63fdc043990e24a128a9a5
LoraVan2 <vanderpuyeloretta22@gmail.com>
2023-03-19 04:25:17 -0700

    printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,14 +2,17 @@
 
 
 /**
- * main - prints "proper grammar, but the outcome is 
+ * main - prints "proper grammar, but the outcome is
  * a piece of art," followed newline
  * Return: Always 0.
  *
  */
 
-int main(void) 
+int main(void)
 
 {
 	printf("proper grammar, but the outcome is a piece of art,\n");
+
 	return (0);
+
+}
